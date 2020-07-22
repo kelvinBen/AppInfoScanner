f5a7afdd8597f6e6c0fc94a0a677a80ae61f1371
kelvin_ben <>
2020-07-22 09:52:57 +0800

    add code to git

--- README.md
+++ README.md
@@ -1,2 +1,158 @@
-# AppInfoScanner
+### AppInfoScanner
+
 一款适用于(Android、iOS、WEB、H5、静态网站)，信息检索的工具，可以帮助渗透测试人员快速获取App或者WEB中的有用资产信息。
+
+### 适用场景
+- 日常渗透测试中对APP中的URL地址、IP地址、关键信息进行采集
+- 大型攻防演练场景中对APP中URL地址、IP地址、关键信息进行采集
+- 对WEB网站源代码进行URL地址、IP地址、关键信息进行采集(可以是开源的代码也可以是右击网页源代码另存为)
+
+
+### 功能介绍:
+- 支持目录批量扫描
+- 支持DEX、APK、IPA、HTML、JS、Smali等文件的静态资源采集
+- 支持自定义扫描规则
+- 支持IP地址信息采集
+- 支持URL地址信息采集
+- 支持中间件信息采集
+- 支持多线程
+- 支持忽略资源文件采集
+- 支持Android包名采集
+
+### 环境说明
+
+- Apk文件解析需要使用JAVA环境,JAVA版本1.8及以下
+- Python3的运行环境
+
+### 目录说明
+
+AppInfoScanner
+    |-- libs  程序的核心代码
+        |-- core
+            |-- parses.py 用于解析文件中的静态信息
+        |-- task
+            |-- android_task.py 用于处理Android相关的文件
+            |-- ios_task.py 用于处理iOS相关的文件
+            |-- web_task.py 用于处理Web相关的文件，比如网页右键源代码、H5相关的静态信息
+    |-- tools 程序需要依赖的工具
+        |-- apktool.jar 用于反编译apk文件
+        |-- baksmali.jar 用于反编译dex文件
+        |-- strings.exe 用于windows 32下获取iPA的字符串信息
+        |-- strings64.exe 用于windows 64的系统获取iPA的字符串信息
+    |-- app.py 主运行程序
+    |-- config.py 用于自定义相关规则
+    |-- readme.md  程序使用说明
+
+
+### Android 相关操作说明
+
+#### 扫描指定的apk
+
+```
+python3 app.py android -i <Your apk file>  
+```
+
+#### 扫描指定的dex
+
+```
+python3 app.py android -i <Your dex file> 
+```
+
+#### 扫描一个目录下所有的APK或者dex
+
+```
+python3 app.py android -i <Your apk or dex directory> 
+```
+
+#### 扫描指定关键字(临时)
+
+```
+python3 app.py android -i <Your apk or dex directory> -r <the keyword>
+```
+
+#### 扫描的时候忽略资源文件
+
+```
+python3 app.py android -i <Your apk or dex directory> -n
+```
+
+#### 扫描指定包下的内容
+```
+python3 app.py android -i <Your apk or dex directory> -p <package1.package2>
+```
+
+#### 扫描所有的字符串
+
+```
+python3 app.py android -i <Your apk or dex directory>  -a
+```
+
+#### 指定线程数量
+
+```
+python3 app.py android -i <Your apk or dex directory> -t 10
+``` 
+
+### iOS 相关操作说明
+
+#### 扫描指定的iPA文件
+
+```
+python3 app.py ios -i <Your ipa file>
+```
+
+#### 扫描指定关键字(临时)
+
+```
+python3 app.py ios -i <Your ipa file> -r  <the keyword>
+```
+
+#### 扫描的时候忽略资源文件
+
+```
+python3 app.py ios -i <Your ipa file> -n
+```
+
+#### 输出所有的字符串
+
+```
+python3 app.py ios -i <Your ipa file> -a
+```
+
+#### 指定线程数量
+
+```
+python3 app.py ios -i <Your ipa file> -t 10
+```
+
+### Web 相关操作说明
+
+#### 扫描指定的Web网站目录或者html相关文件
+```
+python3 app.py web -i <Your website directory> 
+```
+
+#### 扫描指定关键字(临时)
+
+```
+python3 app.py web -i  <Your website directory> -r <the keyword>
+```
+
+#### 输出所有的字符串
+```
+python3 app.py web -i  <Your website directory> -a
+```
+
+
+#### 指定线程数量
+```
+python3 app.py web -i <Your website directory> -t 10
+```
+
+
+### 常见问题
+
+- 1. 信息检索垃圾数据过多？
+
+> 方法1： 根据实际情况调整config.py中的规则信息
+> 方法2： 忽略资源文件
---
+++ libs/task/ios_task.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+# Author: kelvinBen
+# Github: https://github.com/kelvinBen/AppInfoScanner
+
+
+import os
+import re
+import zipfile
+import libs.core as cores
+from queue import Queue
+from libs.core.parses import ParsesThreads
+
+
+class iOSTask(object):
+    thread_list =[]
+    result_dict = {}
+
+    def __init__(self,input, rules, net_sniffer,no_resource,all,threads):
+        self.path = input
+        self.rules = rules
+        self.net_sniffer = net_sniffer
+        self.no_resource = no_resource
+        self.all = all
+        self.threads = threads
+        if rules:
+            config.filter_strs.append(r'.*'+str(rules)+'.*')
+        self.file_queue = Queue()
+        self.shell_falg=False
+
+    def start(self):
+        # ipa 文件
+        if self.path.split(".")[1] == 'ipa':
+            # 对ipa进行解包
+            self.__decode_ipa__(cores.output_path)
+
+            #文件提取
+            self.__scanner_file_by_ipa__(cores.output_path)
+        
+        self.__start_threads()
+        
+        for thread in self.thread_list:
+            thread.join()
+
+        self.__print__()
+        
+
+    def __scanner_file_by_ipa__(self,output):   
+        scanner_file_suffix = ["plist","js","xml","html"]
+        scanner_dir =  os.path.join(output,"Payload")
+        self.__get_scanner_file__(scanner_dir,scanner_file_suffix)
+
+    def __get_scanner_file__(self,scanner_dir,file_suffix):
+        dir_or_files = os.listdir(scanner_dir)
+        for dir_file in dir_or_files:
+            dir_file_path = os.path.join(scanner_dir,dir_file)
+            if os.path.isdir(dir_file_path):
+                if dir_file.endswith(".app"):
+                    self.elf_file_name = dir_file.split(".")[0]
+                self.__get_scanner_file__(dir_file_path,file_suffix)
+            else:
+                if self.elf_file_name == dir_file:
+                    self.file_queue.put(dir_file_path)
+                    continue
+                if self.no_resource:    
+                    dir_file_suffix =  dir_file.split(".")
+                    if len(dir_file_suffix) > 1:
+                        if dir_file_suffix[1] in file_suffix:
+                            self.file_queue.put(dir_file_path)
+
+    def __decode_ipa__(self,output_path):
+        zip_files = zipfile.ZipFile(self.path)
+        for zip_file in zip_files.filelist:
+            zip_files.extract(zip_file.filename,output_path)
+
+    def __start_threads(self):
+        for threadID in range(1,self.threads) : 
+            name = "Thread - " + str(threadID)
+            thread =  ParsesThreads(threadID,name,self.file_queue,self.all,self.result_dict)
+            thread.start()
+            self.thread_list.append(thread)
+    
+
+    def __print__(self):
+        print("=========The result set for the static scan is shown below:===============")
+        with open(cores.result_path,"a+") as f:
+            for key,value in self.result_dict.items():
+                f.write(key+"\r")
+                for result in value:
+                    print(result)
+                    f.write("\t"+result+"\r")
+        print("For more information about the search, see: %s" %(cores.result_path))
+
+        if self.shell_falg:
+            print('\033[1;33mWarning: This application has shell, the retrieval results may not be accurate, Please remove the shell and try again!')
